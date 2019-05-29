48b4a4601ffa5531daac1fc0ef9f515c0f48c923
lightsun <978136772@qq.com>
2019-05-29 10:01:06 +0800

    init

---
+++ SuperAdapter/app/src/main/java/com/heaven7/core/adapter/demo/TestQtActivity.java
@@ -0,0 +1,17 @@
+package com.heaven7.core.adapter.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vida.android.util.Reflection;
+
+public class TestQtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println(new Reflection().hello());
+    }
+}
