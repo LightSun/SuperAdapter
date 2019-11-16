fed55c4776c5e309358b8a242575634ba3eba404
LightSun <donshine723@gmail.com>
2019-11-17 00:19:11 +0800

    opt

--- SuperAdapter/adapter/src/main/java/com/heaven7/adapter/applier/ElevationViewApplier.java
+++ SuperAdapter/adapter/src/main/java/com/heaven7/adapter/applier/ElevationViewApplier.java
@@ -1,7 +1,7 @@
 package com.heaven7.adapter.applier;
 
-import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.view.View;
 
 import com.heaven7.adapter.ISelectable;
@@ -11,7 +11,6 @@ import com.heaven7.adapter.RecycleViewComponentsAdapter;
  * Created by heaven7 on 2019/3/11.
  * @since 2.0.0
  */
-@TargetApi(21)
 public class ElevationViewApplier extends BaseViewApplier implements RecycleViewComponentsAdapter.TwoStateApplier {
 
     private final float selectElevation;
@@ -25,6 +24,8 @@ public class ElevationViewApplier extends BaseViewApplier implements RecycleView
 
     @Override
     protected void apply(Context context, Object contextData, int position, ISelectable item, View view) {
-        view.setElevation(item.isSelected() ? selectElevation : unselectElevation);
+        if(Build.VERSION.SDK_INT >= 21){
+            view.setElevation(item.isSelected() ? selectElevation : unselectElevation);
+        }
     }
 }
